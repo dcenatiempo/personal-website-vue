c821709d14bea12b64d19819294b82829ba1f494
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2020-09-27 12:35:58 -0600

    autoload bas components

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   target: 'static',
   srcDir: 'src/',
   css: ['assets/css/normalize.css', 'assets/css/global.scss'],
-  plugins: [],
+  plugins: ['~/plugins/BaseComponents/index.js'],
   storybook: {
     addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
   },
--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,9 +1,42 @@
 <template>
-  <button>Hello world</button>
+  <button class="base-button" @click="$emit('click', $event)">
+    <slot />
+  </button>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped></style>
+<style lang="scss">
+button {
+  position: relative;
+  font-family: 'Averia Libre', sans-serif;
+  padding: 0 25px;
+  margin: 10px;
+  height: 48px;
+  background-color: #3cc26d;
+  border: 0px solid #4caf50;
+  border-radius: 3px;
+  color: white;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+  &:focus-within::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-radius: 3px;
+    box-shadow: 0 0 10px 3px rgba(130, 172, 62, 0.2);
+  }
+}
+
+button a {
+  color: white;
+}
+</style>
