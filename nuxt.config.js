52185f0a02b2036c99924cb82666c8812df9ddb9
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2020-09-27 12:35:23 -0600

    add global css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 export default {
   target: 'static',
   srcDir: 'src/',
+  css: ['assets/css/normalize.css', 'assets/css/global.scss'],
+  plugins: [],
   storybook: {
     addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
   },
